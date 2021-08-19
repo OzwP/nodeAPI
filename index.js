4da55f10a2976e3f394a621757e186c09dcebf8c
Oswaldo <>
2021-08-18 20:06:14 -0500

    *first commit*

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+/*
+	Verbos HTTP
+	-GET
+	-POST
+	-PATCH
+	-PUT
+	-DELETE
+
+*/
+
+app.get("/", (rec, res, next) => {
+	res.status(200)
+	res.send("Hola Mundo")
+})
+
+app.listen(3000, () => {
+	console.log("Server is running...")
+})
