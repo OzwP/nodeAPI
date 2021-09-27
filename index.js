3f3b54efdaa563e32a1dc8530b75a3c269d71fa9
Oswaldo <>
2021-09-27 14:07:09 -0500

    *added* autorizacion de usuario para acceder a /pokemon

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // const bodyParser = require('body-parser');
 
+// Dependencias
 const express = require("express");
 const app = express();
 const morgan = require("morgan")
+// Routers
 const pokemon = require("./routes/pokemon")
 const user = require("./routes/user")
+// Middleware
+const auth = require("./middleware/auth")
+const notFound = require("./middleware/notFound")
+const index = require("./middleware/index")
 
 app.use(morgan("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/", (req, res, next) => {
-	res.status(200).json({code: 1, message: "Bienvenido al pokedex"})
-})
-
-app.use("/pokemon",pokemon)
+app.get("/", index)
 app.use("/user", user)
-
-app.use((req,res,next) => {
-	return res.status(404).json({code : 404, message : "URL no encontrada"})
-})
+app.use(auth)
+app.use("/pokemon",pokemon)
+app.use(notFound)
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running...")
